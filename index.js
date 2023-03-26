3b822bfbaaf969336d5b9369acb0eab000a50bf9
Ibrahim Munassar <him072019@gmail.com>
2023-03-26 12:11:47 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction (spy) {
+    return spy()
+}
+function returnsANamedFunction(){
+    return function sayHello(){
+        console.log("Hello");
+    }
+}
+function returnsAnAnonymousFunction() {
+    return function(){
+        return "I'm an anonymous function"
+    }
+}
